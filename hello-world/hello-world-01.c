2c293e9a5da2795db502d206bcdd3fe2b1d0c615
Shrikant Bobade <bobadeshrikant@gmail.com>
2017-02-06 01:28:03 +0530

    hello-world-01.c: simple kernel module
    this kenel module contains just init and exit functions of LKM.
    
    Signed-off-by: Shrikant Bobade <bobadeshrikant@gmail.com>

--- hello-world/hello-world-01.c
+++ hello-world/hello-world-01.c
@@ -2,17 +2,17 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
-int example_01_simple_module_init(void)
+int hello_world_01_init(void)
 {
 	printk(KERN_ALERT "This function is %s at line %d \n", __FUNCTION__, __LINE__);
 	return 0;
 }
 
 
-void example_01_simple_module_exit(void)
+void hello_world_01_exit(void)
 {
 	printk(KERN_ALERT "This function is %s at line %d \n", __FUNCTION__, __LINE__);
 }
 
-module_init(example_01_simple_module_init);
-module_exit(example_01_simple_module_exit);
+module_init(hello_world_01_init);
+module_exit(hello_world_01_exit);
