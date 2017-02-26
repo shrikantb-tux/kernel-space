7c95578692b6cb456f57dc63abe75a19a9a1aa92
Shrikant Bobade <bobadeshrikant@gmail.com>
2017-02-27 00:01:30 +0530

    hello-world-01.c: add o/p section
    Signed-off-by: Shrikant Bobade <bobadeshrikant@gmail.com>

--- hello-world/hello-world-01.c
+++ hello-world/hello-world-01.c
@@ -16,3 +16,14 @@ void hello_world_01_exit(void)
 
 module_init(hello_world_01_init);
 module_exit(hello_world_01_exit);
+
+# if 0
+This is quick section gives real o/p inserting and removing the simple module
+o/p
+
+Feb 26 23:58:53 shrikant-VirtualBox kernel: [ 6297.531271] hello_world_01: module license 'unspecified' taints kernel.
+Feb 26 23:58:53 shrikant-VirtualBox kernel: [ 6297.531275] Disabling lock debugging due to kernel taint
+Feb 26 23:58:53 shrikant-VirtualBox kernel: [ 6297.534579] This function is hello_world_01_init at line 7 
+Feb 26 23:59:22 shrikant-VirtualBox kernel: [ 6326.815988] This function is hello_world_01_exit at line 14 
+
+#endif
